633ec702f556ba78040cc0092b7326a58114af87
Gilad <gilad.smith@yahoo.com>
2023-04-26 21:40:50 +0100

    added ability to sort along columns and rows changed some variable names to make it more clear

--- main.py
+++ main.py
@@ -6,30 +6,34 @@ img = Image.open('Bamburgh_Castle,_beautiful_day.jpg')
 img_a = np.array(img)
 
 
-def sort_column(column):
+def sort_row(row):
     # sums all the pixel values to find a basic pixel "brightness" value
-    summed = np.sum(column[:,:4], axis=1)
+    summed = np.sum(row[:, :4], axis=1)
     min_index = np.argmin(summed)
-    print(min_index)
 
     # sorts the row up to min_index
-    sorted_section = np.argsort(column[:min_index + 1].sum(axis=1))
+    sorted_section = np.argsort(row[:min_index + 1].sum(axis=1))
     # creates the final array by concatenating the new section with the old section
-    final_arr = np.concatenate((column[:min_index + 1][sorted_section], column[min_index + 1:]), axis=0)
+    final_arr = np.concatenate((row[:min_index + 1][sorted_section], row[min_index + 1:]), axis=0)
     return final_arr
 
 
-test = sort_column(img_a[0])
+test = sort_row(img_a[0])
 
 
-
-def sort_columns(image):
+def pixelsort(image, column=False):
+    if column:
+        image = np.rot90(image)
     new_array = np.empty_like(image)
     for i, column in enumerate(image):
-        new_array[i] = sort_column(column)
+        new_array[i] = sort_row(column)
 
     return new_array
 
-img_b = sort_columns(img_a)
+
+column = True
+img_b = pixelsort(img_a, column=column)
+if column:
+    img_b = np.rot90(img_b, k=3)
 img_b = Image.fromarray(img_b)
 img_b.save('sorted.jpg')
