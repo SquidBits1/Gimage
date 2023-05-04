eea60d2deceff99aa321fa058a4bf5c02b02a4ed
Gilad <gilad.smith@yahoo.com>
2023-05-04 22:28:46 +0100

    convolution is starting to make sense

--- kimasendorf method/kimasendorf.py
+++ kimasendorf method/kimasendorf.py
@@ -10,3 +10,8 @@ grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, low = cv2.threshold(grey, thresh_low, 255, cv2.THRESH_BINARY)
 ret2, high = cv2.threshold(grey, thresh_high, 255, cv2.THRESH_BINARY_INV)
 full_mask = cv2.bitwise_and(low, high)
+
+a = lambda row: np.convolve(row, [-1, 1], 'same')
+
+edges = np.apply_along_axis(a,0,full_mask)
+
