ec05ee22e026e34beec57210914a4d89e1ab3ae2
Gilad <gilad.smith@yahoo.com>
2023-05-09 20:45:29 +0100

    got some basic thresholding working!
    Took 1 hour 0 minutes

--- Main Files/Thresholding/threshold.py
+++ Main Files/Thresholding/threshold.py
@@ -1,4 +1,30 @@
 from PIL import Image
 import numpy as np
-img = Image.open("C:/Users/gilsmi0809/PycharmProjects/pixel_sorting/Main Files/test_images\castle.jpg")
-img_a = np.array(img)
+
+img = Image.open("castle.jpg")
+img = np.array(img)
+
+
+# test = np.array([[[255, 0, 0], [34, 111, 0]], [[0, 0, 225], [1, 1, 141]]])
+
+
+def img2grey(image):
+    grey = np.dot(image[..., 0:3], [0.299, 0.587, 0.114])
+    return grey
+
+
+def binary_threshold(image, threshold):
+    image = img2grey(image)
+    for cell in np.nditer(image, op_flags=['readwrite']):
+        if cell > threshold:
+            cell[...] = 255
+        else:
+            cell[...] = 0
+
+    return image
+
+
+img = binary_threshold(img, 190)
+print(img.shape)
+img = Image.fromarray(img).convert('RGB')
+img.save('threshold.png')
