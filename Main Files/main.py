ae35276ce7bc047cb18af56e9a8cf06319423b90
Gilad <gilad.smith@yahoo.com>
2023-09-20 09:31:19 +0100

    Added main file where it all comes together

---
+++ Main Files/main.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append("GUI")
+sys.path.append("Pixel Sorting")
+sys.path.append("Thresholding")
+
+import test
+
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
